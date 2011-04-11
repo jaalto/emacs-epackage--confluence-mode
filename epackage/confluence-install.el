25e7ddcc251ca489e8cbe61cccd9c219896d53c5
Jari Aalto <jari.aalto@cante.net>
2011-04-11 11:27:22 +0300

    epackage/confluence-install.el: new

---
+++ epackage/confluence-install.el
@@ -0,0 +1,9 @@
+;; Subset of user functions
+(autoload 'confluence-get-page                  "confluence" "" t)
+(autoload 'confluence-newline-and-indent        "confluence" "" t)
+(autoload 'confluence-list-indent-dwim          "confluence" "" t)
+(autoload 'confluence-modify-list-indent        "confluence" "" t)
+(autoload 'confluence-ediff-merge-current-page  "confluence" "" t)
+(autoload 'confluence-ediff-current-page        "confluence" "" t)
+
+(provide 'confluence-install)
