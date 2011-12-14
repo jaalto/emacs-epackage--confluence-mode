630bf7d4f998a1575583742ac72c05d2741ce82a
Jari Aalto <jari.aalto@cante.net>
2011-12-14 15:21:38 +0200

    confluence-mode-epkg-compile.el: new

---
+++ epackage/confluence-mode-epkg-compile.el
@@ -0,0 +1,9 @@
+(dolist (file
+         '("confluence-edit.el"
+	   "confluence.el"))
+  (if (and (boundp 'verbose)
+	   verbose)
+      (message "Byte Compile %s" file))
+  (if (file-exists-p file)
+      (byte-compile-file file)
+    (message "** Byte compile error. Not found: %s" file)))
